e92f05502e6ddbca2743b76b4e92399b5dc7ffe3
Dom Rodriguez <shymega@shymega.org.uk>
2016-08-10 14:25:02 +0100

    Update obsolete gotour URI to new one

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ echo "Create folders.."
 mkdir -p $GOPATH
 
 echo "Install Go Tour.."
-go get code.google.com/p/go-tour/gotour
+go get golang.org/x/tour/gotour
 
 echo "Set permissions.."
 chown -R gotour:gotour /home/gotour
